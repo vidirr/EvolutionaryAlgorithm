a0735e80729ba7b1f5ca2f8cb4895a384f0b19ff
Víðir Orri Reynisson <vidirr@gmail.com>
2013-11-18 11:21:45 +0000

    Moved algorithm implementation to AlgorithmFactory.

--- AlgorithmFactory/Algorithms.py
+++ AlgorithmFactory/Algorithms.py
@@ -1,11 +1,26 @@
+from TestsFactory import Tests
 
+#USED INSTEAD OF RANDOM
+from MTrandom.MTrandom import  MersenneTwister as MT
+mt = MT()
 
+def BEA(N, xmin, xmax, optimum, iters=1000):
+	"""  
+		Implementation of the BasicEvolutionaryAlgorithm as presented
+		in the slides.
 
-def BasicEvolutionaryAlgorithm(N, xmin, xmax, optimum):
-"""  
-	Implementation of the basic evolutionary algorithm as presented
-	in the slides.
+	"""
 
-"""
+	cnt, Done = 0, False
+
+	#Initial population
+	P = [mt.uniform(xmin, xmax) for _ in range(N)]
+	
+	while cnt < iters and not Done:
+		ans = Tests.f1(P)
+		print ans
+		return
+		#Select parent pool from P
+
 
 	return "foobang"
